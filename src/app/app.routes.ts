20d92ebb0e0ef5410a546bd6f006821e80408a8a
Vivek Panchal <ivivekpanchal123@gmail.com>
2024-07-19 22:23:09 +0530

    [ADD] misc: added many components, dependencies, etc.
    Introduced Dependencies:
    - Angular Material
    - Bootstrap
    
    Implemented Components:
    - Navbar Component: Added as a base component instead of router.
    - Sign In Component: New UI with combined signin and signup
    functionality.
    - Password-Manager: Base layout with table view, including a dialog form
    for adding new passwords and new services for API handling.
    
    Modifications:
    - Introduced new routes for better application flow.
    
    NOTE: SOMETHING BIG IS COMING. 😉

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { SignInComponent } from './components/sign-in/sign-in.component';
+import { PasswordManagerComponent } from './components/apps/password-manager/password-manager.component';
 
 export const routes: Routes = [
-  {path : "", component: SignInCo mponent }
+  {path : "", component: SignInComponent },
+  {path : "pass", component: PasswordManagerComponent }
 ];
